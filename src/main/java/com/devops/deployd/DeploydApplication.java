2a7ffdb870e7741f8bb5d18c3eb44bc517d18c39
shikha1982 <shikha1982@gmail.com>
2020-06-04 23:54:20 -0500

    test commit

---
+++ src/main/java/com/devops/deployd/DeploydApplication.java
@@ -0,0 +1,13 @@
+package com.devops.deployd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploydApplication.class, args);
+	}
+
+}
